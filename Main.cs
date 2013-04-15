e2f16ea197ed185437c79f443949ef8c1ea65687
Andy P. <naito_p@yahoo.co.jp>
2013-04-15 10:03:54 +0900

    Minor fixes

--- Main.cs
+++ Main.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-//using System.ComponentModel;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
 using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
@@ -22,9 +17,9 @@ namespace Transposer
 
         private Dictionary<string, string> _parameters = new Dictionary<string, string>();
         private readonly List<BloombergSecurity> _securities = new List<BloombergSecurity>();
-        private List<string> Fields = new List<string>();
+        private List<string> _fields = new List<string>();
         private DataTable _transposerTable = new DataTable();
-        private BloombergRealTimeData _bloombergRealTimeData = new BloombergRealTimeData();
+        private readonly BloombergRealTimeData _bloombergRealTimeData = new BloombergRealTimeData();
         public DataGridView DataGridViewTrnspsr;
 
 
@@ -61,7 +56,7 @@ namespace Transposer
 
             var bindingSource1 = new BindingSource {DataSource = _transposerTable};
             dataGridViewTrnspsr.DataSource = bindingSource1;
-            bindingSource1.ListChanged += new ListChangedEventHandler(bindingSource1_ListChanged); 
+            bindingSource1.ListChanged += bindingSource1_ListChanged; 
 
 
             FormatDatagrid();
@@ -69,24 +64,32 @@ namespace Transposer
 
         public void bindingSource1_ListChanged(object sender, ListChangedEventArgs e)
         {
-            try
+            if (InvokeRequired)
             {
-                if (InvokeRequired)
-                {
-                    var d = new SetTextCallback(bindingSource1_ListChanged);
-                    Invoke(d, new object[] {sender, e});
-                }
-                else
-                {
+                var d = new SetTextCallback(bindingSource1_ListChanged);
+                Invoke(d, new[] {sender, e});
+            }
+            else
+            {
 
-                    if (String.Equals(e.PropertyDescriptor.Name, BckClrCol))
+                if (String.Equals(e.PropertyDescriptor.Name, BckClrCol))
+                {
+                    int direction;
+                    var cellStyle = new DataGridViewCellStyle();
+                    int.TryParse(_transposerTable.Rows[e.NewIndex][BckClrCol].ToString(), out direction);
+                    if (direction < 0)
                     {
-                        int direction;
-                        var cellStyle = new DataGridViewCellStyle();
-                        int.TryParse(_transposerTable.Rows[e.NewIndex][BckClrCol].ToString(), out direction);
-                        if (direction < 0)
+                        cellStyle.BackColor = Color.Red;
+                        for (int i = 0; i < _dataGridColCnt; i++)
                         {
-                            cellStyle.BackColor = Color.Red;
+                            dataGridViewTrnspsr.Rows[e.NewIndex].Cells[i].Style = cellStyle;
+                        }
+                    }
+                    else
+                    {
+                        if (direction > 0)
+                        {
+                            cellStyle.BackColor = Color.LawnGreen;
                             for (int i = 0; i < _dataGridColCnt; i++)
                             {
                                 dataGridViewTrnspsr.Rows[e.NewIndex].Cells[i].Style = cellStyle;
@@ -94,29 +97,14 @@ namespace Transposer
                         }
                         else
                         {
-                            if (direction > 0)
+                            cellStyle.BackColor = Color.White;
+                            for (int i = 0; i < _dataGridColCnt; i++)
                             {
-                                cellStyle.BackColor = Color.LawnGreen;
-                                for (int i = 0; i < _dataGridColCnt; i++)
-                                {
-                                    dataGridViewTrnspsr.Rows[e.NewIndex].Cells[i].Style = cellStyle;
-                                }
-                            }
-                            else
-                            {
-                                cellStyle.BackColor = Color.White;
-                                for (int i = 0; i < _dataGridColCnt; i++)
-                                {
-                                    dataGridViewTrnspsr.Rows[e.NewIndex].Cells[i].Style = cellStyle;
-                                }
+                                dataGridViewTrnspsr.Rows[e.NewIndex].Cells[i].Style = cellStyle;
                             }
                         }
                     }
                 }
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine("BG Chg error: {0}", exception.Message);
             }
         }
 
@@ -130,20 +118,20 @@ namespace Transposer
             var name = transposerTable.Columns.Add("Name", typeof(string));
             name.Unique = true;
 
-            foreach (var field in Fields)
+            foreach (var field in _fields)
             {
                 transposerTable.Columns.Add(field, typeof(double));
             }
 
-            var change = transposerTable.Columns.Add("Change", typeof(string));
-            var highlight = transposerTable.Columns.Add(BckClrCol, typeof(int));
+            transposerTable.Columns.Add("Change", typeof(string));
+            transposerTable.Columns.Add(BckClrCol, typeof(int));
 
             return transposerTable;
         }
 
         private void SetFields()
         {
-            Fields = new List<string>() { "Mid", "Bid", "Ask" };
+            _fields = new List<string> { "Mid", "Bid", "Ask" };
         }
 
         private void FormatDatagrid()
@@ -201,7 +189,7 @@ namespace Transposer
             {
                 DataGridViewRow dataGridrow = dataGridViewTrnspsr.Rows[i];
                 DataRow dataRow = _transposerTable.Rows[i];
-                var security = new BloombergSecurity(dataGridrow, dataRow, Fields, this);
+                var security = new BloombergSecurity(dataGridrow, dataRow, _fields, this);
                 _securities.Add(security);
                 _bloombergRealTimeData.AddSecurity(security);
             }
@@ -215,21 +203,21 @@ namespace Transposer
             {
                 using (var sr = new StreamReader(path))
                 {
-                    string line = "";
+                    string line;
                     while ((line = sr.ReadLine()) != null)
                     {
                         string[] items = line.Split(';');
-                        if (items[0].ToString().Substring(0, 1) != "#")
+                        if (items[0].Substring(0, 1) != "#")
                         {
                             parsedText.Add(items[0], items[1]);
-                            Console.WriteLine("{0} {1}", items[0], items[1]);
+                            Console.WriteLine(@"{0} {1}", items[0], items[1]);
                         }
                     }
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(@"The file could not be read:");
                 Console.WriteLine(e.Message);
             }
 
@@ -302,19 +290,25 @@ namespace Transposer
             public static int CountGridWidth(DataGridView dgv)
             {
                 int width = 0;
-                foreach (DataGridViewColumn column in dgv.Columns)
-                    if (column.Visible == true)
+                for (int index = 0; index < dgv.Columns.Count; index++)
+                {
+                    DataGridViewColumn column = dgv.Columns[index];
+                    if (column.Visible)
                         width += column.Width;
-                return width += 2;
+                }
+                return width + 2;
             }
 
             public static int CountGridHeight(DataGridView dgv)
             {
                 int width = 0;
-                foreach (DataGridViewRow rows in dgv.Rows)
-                    if (rows.Visible == true)
+                for (int index = 0; index < dgv.Rows.Count; index++)
+                {
+                    DataGridViewRow rows = dgv.Rows[index];
+                    if (rows.Visible)
                         width += rows.Height;
-                return width += 44;
+                }
+                return width + 44;
             }
         }
 
